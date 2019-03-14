edc6bdc06a87582353c13315e12cc7b4a36679d2
Luis Moral Guerrero <luis.moral@gmail.com>
2019-03-14 18:28:17 +0100

    Initial import.

---
+++ src/test/java/es/molabs/challengeblueorangeapi/CreatorFeature.java
@@ -0,0 +1,15 @@
+package es.molabs.challengeblueorangeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreatorFeature {
+
+	@Test
+	public void contextLoads() {
+	}
+}
