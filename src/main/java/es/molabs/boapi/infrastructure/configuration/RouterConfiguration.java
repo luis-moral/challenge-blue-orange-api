d7e5a6ee6f5369f3cfd71431946f303a934498e2
Luis Moral Guerrero <luis.moral@gmail.com>
2019-03-16 04:17:06 +0100

    Cleaning.

--- src/main/java/es/molabs/boapi/infrastructure/configuration/RouterConfiguration.java
+++ src/main/java/es/molabs/boapi/infrastructure/configuration/RouterConfiguration.java
@@ -4,7 +4,10 @@ import es.molabs.boapi.infrastructure.handler.CreatorHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class RouterConfiguration {
